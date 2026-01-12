d66b4d152d84699b4a41e29e8d57c4adfb56b855
Liam McCaul <lmccaul@hotmail.com>
2026-01-12 21:12:23 +0000

    Imported current list of required imports. Completed simple startup function to print hello world.

---
+++ Fundamentals_Solution.py
@@ -0,0 +1,12 @@
+import csv, os
+from datetime import datetime
+
+CSV_PATH = "improved_inventory.csv"
+COLUMNS = ["itemId", "itemName", "quantity"]
+
+
+def StartUp() : 
+    print("Hello World!")
+
+
+StartUp()
